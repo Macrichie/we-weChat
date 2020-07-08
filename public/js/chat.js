df68f2e770aa813ba7948f345f9eabc8de32d3c2
Macrichie <makanjuolakunle@gmail.com>
2020-07-08 03:27:21 +0100

    initial commit, created src and public directories with their respective files to run server

---
+++ public/js/chat.js
@@ -0,0 +1,108 @@
+const socket = io()
+
+const $messageForm = document.querySelector('#form-data')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+const $locationButton = document.querySelector('#send-location')
+const $messages = document.querySelector("#messages")
+
+// Templates
+const messageTemplate = document.querySelector("#message-template").innerHTML
+const locationTemplate = document.querySelector("#location-template").innerHTML
+const sidebarTemplate = document.querySelector('#sidebar-template').innerHTML
+
+// Options
+const { username, room } = Qs.parse(location.search, { ignoreQueryPrefix: true })
+
+const autoscroll = () => {
+    // New message element
+    const $newMessage = $messages.lastElementChild
+
+    // Height of the new message
+    const newMessageStyles = getComputedStyle($newMessage)
+    const newMessageMargin = parseInt(newMessageStyles.marginBottom)
+    const newMessageHeight = $newMessage.offsetHeight + newMessageMargin
+
+    // Visible height
+    const visibleHeight = $messages.offsetHeight
+
+    // Height of messages container
+    const containerHeight = $messages.scrollHeight
+
+    // How far have I scrolled?
+    const scrollOffset = $messages.scrollTop + visibleHeight
+
+    if(containerHeight - newMessageHeight <= scrollOffset) {
+        $messages.scrollTop = $messages.scrollHeight
+    }
+}
+
+socket.on("message", msg => {
+    console.log(msg)
+    const html = Mustache.render(messageTemplate, {
+        username: msg.username,
+        msg: msg.text,
+        createdAt: moment(msg.createdAt).format('h:mm a')
+    })
+    $messages.insertAdjacentHTML("beforeend", html)
+    autoscroll()
+})
+
+socket.on("locationMessage", data => {
+    console.log(data)
+    const html = Mustache.render(locationTemplate, {
+        username: data.username,
+        url: data.url,
+        createdAt: moment(data.createdAt).format('h:mm a')
+    })
+    $messages.insertAdjacentHTML("beforeend", html)
+    autoscroll()
+})
+
+socket.on('roomData', ({ room, users }) => {
+    const html = Mustache.render(sidebarTemplate, {
+        room,
+        users
+    })
+    document.querySelector('#sidebar').innerHTML = html
+})
+
+// sending from client to server
+$messageForm.addEventListener('submit', e => {
+    e.preventDefault(e)
+    $messageFormButton.setAttribute('disabled', 'disabled')
+    const message = e.target.elements.message.value
+
+    socket.emit('sendMessage', message, (acknowledge) => {
+        $messageFormButton.removeAttribute('disabled')
+        $messageFormInput.value = ''
+        $messageFormInput.focus()
+        console.log("✔️", acknowledge)
+
+    })
+})
+
+document.querySelector("#send-location").addEventListener('click', () => {
+    //incase there is no geolocation api access
+    if(!navigator.geolocation) {
+        return alert("Geolocation is not supported on your browser")
+    }
+    $locationButton.setAttribute('disabled', 'disabled')
+    navigator.geolocation.getCurrentPosition(position => {
+        const {coords: {latitude,longitude}} = position
+        socket.emit("sendLocation", {
+            latitude,
+            longitude
+        }, () => {
+            console.log("Location shared!")
+            $locationButton.removeAttribute('disabled')
+        })
+    })
+})
+
+socket.emit('join', { username, room }, (error) => {
+    if(error) {
+        alert(error)
+        location.href = "/"
+    }
+})
---
+++ src/utils/users.js
@@ -0,0 +1,63 @@
+const users = []
+
+// addUser, removeUser, getUser, getUsersInRoom
+
+const addUser = ({ id, username, room }) => {
+    // Clean the data
+    username = username.trim().toLowerCase()
+    room = room.trim().toLowerCase()
+
+    // Validate the data
+    if(!username || !room) {
+        return {
+            error: 'Username and room are required!'
+        }
+    }
+
+    // check for existing user
+    const existingUser = users.find(user => user.room === room && user.username === username)
+
+    // Validate username
+    if(existingUser) {
+        return {
+            error: "Username already taken"
+        }
+    }
+
+    // store user
+    const user = { id, username, room }
+    users.push(user)
+    return { user }
+}
+
+const removeUser = (id) => {
+    const index = users.findIndex(user => user.id === id)
+    
+    if(index !== -1) {
+        return users.splice(index, 1)[0]
+    }
+}
+
+const getUser = (id) => {
+    const user = users.find(person => person.id === id)
+    if(!user) {
+        return {
+            error: "User does not exist"
+        }
+    }
+    return user
+}
+
+const getUsersInRoom = (room) => {
+    return users.filter(user => {
+        return user.room === room
+    })
+}
+
+
+module.exports = {
+    addUser,
+    removeUser,
+    getUser,
+    getUsersInRoom
+}
